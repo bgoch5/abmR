48edaaef273252c1b1e8d5b2ed14bbbfe9a19fbd
Ben Gochanour <42599357+bgoch5@users.noreply.github.com>
2020-11-19 16:55:22 -0600

    Update moveVIZ.R

--- R/moveVIZ.R
+++ R/moveVIZ.R
@@ -38,9 +38,11 @@ moveVIZ=function(data,title="Visualizing MoveSIM results")
     end.p.df <- cbind(end.p.df, run)
     ideal.df <- rbind(start.p.df, end.p.df)
     t.energy.res <- data$results
+    my_xlim = c((min(t.energy.res$lon,na.rm=T)-6), (max(t.energy.res$lon,na.rm=T)+6))
+    my_ylim = c((min(t.energy.res$lat,na.rm=T)-6), (max(t.energy.res$lat,na.rm=T)+6))
     myplot=ggplot(data = world) +
       geom_sf() +
-      coord_sf(xlim = c(-127, -78), ylim = c(10, 55), expand = FALSE) +
+      coord_sf(xlim = my_xlim, ylim = my_ylim, expand = FALSE) +
       geom_path(data = t.energy.res,
                 aes(x=lon, y=lat),
                 color = "blue", size = 0.6, alpha = 0.4, lineend = "round") +
