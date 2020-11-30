0644e4d9aaaddaf9d3fa7ad1b2781efd9641d27e
Ben Gochanour <42599357+bgoch5@users.noreply.github.com>
2020-11-29 18:58:26 -0600

    updates

--- R/energyVIZ.R
+++ R/energyVIZ.R
@@ -16,16 +16,27 @@
 #' @param data Data to be plotted, this object should be the output from
 #' energySIM().
 #' @param title Title for the plot that is output.
-#' @param type Plot, gradient, or table?
+#' @param type String from "plot", "gradient", or "table"?
+#' @param aspect_ratio Aspect ratio, defaults to 4/3
+#' @param label Logical, label the origin and specified final destination?
+#' @param xlim Optionally specify desired x limits as a numeric vector: c(low,hi)
+#' @param ylim Optionally specify desired y limits as a numeric vector: c(low,hi)
 #'
 #' @return
 #' A plot showing model output compared to a dashed line that represents straight line
 #' movement from the starting point to the final destination.
 #' @examples
-#'
+#' testing=energySIM(replicates=2,days=27,env_rast=ndvi_raster, search_radius=375,
+#' sigma=.4, dest_x=-100, dest_y=20, mot_x=1, mot_y=1, 
+#' modeled_species=my_species, my_shapefile=NOAM, optimum_lo=.4,optimum_hi=.6,init_energy=100,
+#' direction="S",write_results=TRUE,single_rast=FALSE)
+#' energyVIZ(testing,title="Visualizing EnergySIM results",type="plot", aspect_ratio=5/3,
+#' label=TRUE)
 #' @export
 
-energyVIZ=function(data,title="Visualizing EnergySIM results",type="plot")
+energyVIZ=function(data,title="Visualizing EnergySIM results",type="plot",
+                   aspect_ratio=4/3, label=FALSE,
+                   xlim=NULL,ylim=NULL)
 {
 if(type=="plot"){
 world <- ne_countries(scale = "medium", returnclass = "sf")
@@ -44,6 +55,46 @@ t.energy.res <- data$results
 
 my_xlim = c((min(t.energy.res$lon,na.rm=T)-6), (max(t.energy.res$lon,na.rm=T)+6))
 my_ylim = c((min(t.energy.res$lat,na.rm=T)-6), (max(t.energy.res$lat,na.rm=T)+6))
+
+#Latitude
+if(my_ylim[1]>ideal.df[2,2]){
+  my_ylim[1]=ideal.df[2,2]-4
+}
+
+if(my_ylim[2]<ideal.df[2,2]){
+  my_ylim[2]=ideal.df[2,2]+4
+}
+
+# Longitude
+if(my_xlim[1]>ideal.df[2,1]){
+  my_xlim[1]=ideal.df[2,1]-4
+}
+
+if(my_xlim[2]<ideal.df[2,1]){
+  my_xlim[2]=ideal.df[2,1]+4
+}
+
+y_diff=abs(my_ylim[2]-my_ylim[1])
+x_diff=abs(my_xlim[2]-my_xlim[1])
+
+if(y_diff>2*x_diff){
+  my_xlim[1]=my_xlim[1]-.5*x_diff
+  my_xlim[2]=my_xlim[2]+.5*x_diff
+}
+
+if(x_diff>2*y_diff){
+  my_ylim[1]=my_ylim[1]-.5*y_diff
+  my_ylim[2]=my_ylim[2]+.5*y_diff
+}
+
+if(!is.null(xlim)){
+  my_xlim=xlim
+}
+
+if(!is.null(ylim)){
+  my_ylim=ylim
+}
+
 myplot=ggplot(data = world) +
   geom_sf() +
   coord_sf(xlim =my_xlim,
@@ -54,8 +105,14 @@ myplot=ggplot(data = world) +
             color = "red", size = 0.6, alpha = 0.4, lineend = "round") +
   geom_path(data = ideal.df,
             aes(x=Lon, y=Lat),
-            color = "black", size = 1.2, alpha = 1, linetype = 2) +
+            color = "black", size = 1.2, alpha = 1, linetype = "dashed") + theme(aspect.ratio=aspect_ratio)
   ggtitle(title)
+if(label){
+  ideal.df[,"type"]=NA
+  ideal.df[1,4]="Origin"
+  ideal.df[2,4]="Ideal Final"
+  myplot=myplot+geom_point(data=ideal.df,aes(x=Lon,y=Lat,color=type))
+}
 return(myplot)
 }
 if(type=="gradient")
